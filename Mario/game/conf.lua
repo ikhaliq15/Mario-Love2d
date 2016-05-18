c65f827a663a561426f8ba2977aad6415d15984a
ikhaliq15 <Imran@MacBook-Pro.local>
2016-05-18 15:21:45 -0700

    Complete Restart of Code.
    I rewrote all the code and used a different camera/tileloader library.
    Now the collision works. Yay.

--- Mario/game/conf.lua
+++
@@ -1,7 +0,0 @@
-function love.conf(t)
-    t.title = "Super Mario Bros"		    -- The title of the window the game is in (string)
-    t.window.width = 256 * 2		    -- The window width (number)
-    t.window.height = 224 * 2			    -- The window height (number)
-    t.window.fullscreen = false		-- Enable fullscreen (boolean)
-    t.console = false
-end
